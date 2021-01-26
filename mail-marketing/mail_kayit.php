2cb51b2aa9cb402756ca930478418caa045f1ee9
Abbas Eren Kılıç <67686692+Aeknasd145@users.noreply.github.com>
2021-01-26 15:41:10 +0300

    Update mail_kayit.php

--- mail-marketing/mail_kayit.php
+++ mail-marketing/mail_kayit.php
@@ -17,7 +17,7 @@
 	 		die;
 		}
 
-		$email_kayit = $wpdb->query("INSERT INTO boywp_comments(comment_post_ID,comment_author,comment_author_email,comment_author_url,comment_author_IP,comment_date,comment_date_gmt,comment_content,comment_karma,comment_approved,comment_agent,comment_type,comment_parent,user_id) VALUES ('0','mail_kayit','$email','','','0000-00-00 00:00:00.000000','0000-00-00 00:00:00.000000','...','0','1','0','comment','0','0')"  );
+		$email_kayit = $wpdb->query("INSERT INTO {$wpdb->prefix}comments(comment_post_ID,comment_author,comment_author_email,comment_author_url,comment_author_IP,comment_date,comment_date_gmt,comment_content,comment_karma,comment_approved,comment_agent,comment_type,comment_parent,user_id) VALUES ('0','mail_kayit','$email','','','0000-00-00 00:00:00.000000','0000-00-00 00:00:00.000000','...','0','1','0','comment','0','0')"  );
 
 	 	if($email_kayit){
 			$ref = $_SERVER['HTTP_REFERER']."&status=1&email=".$email;
